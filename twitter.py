fc1c0c0f5f856f3240eb0f35a5b9237a578d4fae
AvadaCoder <51478968+gheacarbonilla@users.noreply.github.com>
2020-07-08 01:57:28 +0800

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,8 +1,8 @@
 import tweepy
 import time
 
-auth = tweepy.OAuthHandler('cnab5P79PWmBud6D36idavDAh','CV0h5SicEFiFMSCwVGhsC8A94IV12UbQibTvftDuu3kjH0Wrd6')
-auth.set_access_token('1280437755349045248-NScA1PdqjrqtTgw6M9pFaoiSLyiF4Y','tJwQDIn2bh9NE5xj806F4YJUt5HjWR9wKubd3F6qwVQmh')
+auth = tweepy.OAuthHandler('')
+auth.set_access_token('')
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
